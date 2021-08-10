fa07ca9a431e7b1fda478ad53ebd4c6e08c4936b
Akhil Gosala <60161763+Akhildotexe@users.noreply.github.com>
2021-08-10 18:25:38 -0400

    added the database config

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ from discord_components import DiscordComponents
 from datetime import timedelta, datetime
 from discord.ext import commands
 from dotenv import load_dotenv
+import sqlite3
 
 
-client = commands.Bot(command_prefix=(","),help_command=None,activity=discord.Activity(type=discord.ActivityType.listening,name=" to ,help | Ping Me"),intents=discord.Intents.all())
+client = commands.Bot(command_prefix=(","),help_command=None,activity=discord.Activity(type=discord.ActivityType.listening,name=" ,help | Ping Me"),intents=discord.Intents.all())
 
 
-cogs = ('cogs.errors', 'cogs.help cmds', 'cogs.calc', 'cogs.gen cmds','cogs.fun cmds','cogs.image cmds','cogs.mod cmds','cogs.ttt','cogs.botserver','cogs.ticket')
+cogs = ('cogs.errors', 'cogs.help cmds', 'cogs.calc', 'cogs.gen cmds','cogs.fun cmds','cogs.image cmds','cogs.mod cmds','cogs.ttt','cogs.ticket','cogs.botserver')
 if __name__ == '__main__':
   for ex in cogs:
     client.load_extension(ex)
@@ -17,6 +18,7 @@ if __name__ == '__main__':
 
 DiscordComponents(client)
 load_dotenv('.env')
+
 @client.event
 async def on_ready():
     global up_time
@@ -32,5 +34,13 @@ async def uptime(ctx):
     days, hours = divmod(hours, 24)
     embed = discord.Embed(title=f"I've been up for {days}d, {hours}h, {minutes}m, {seconds}s,", color=0xff0000)
     await ctx.reply(embed=embed)
+
+
+conn= sqlite3.connect('./data/Database.db')
+c= conn.cursor()
+
+c.execute('''CREATE TABLE IF NOT EXISTS Tickets(id TEXT PRIMARY KEY, msg INTEGER, channel INTEGER, lmsg INTEGER, author INTEGER, closed INTEGER)''')
+c.execute('''CREATE TABLE IF NOT EXISTS Guild_ticket(id INTEGER PRIMARY KEY, msg INTEGER, num INTEGER, category INTEGER)''')
+conn.commit()
 
 client.run(os.getenv('TOKEN'))
